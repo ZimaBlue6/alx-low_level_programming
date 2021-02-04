50d33688cd037ad72108fc278ee241f3c3541467
Kidus Namaga <kidusrocks@gmail.com>
2021-02-04 18:37:24 +0000

    further on C

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-  int n, k;
-for (n = 48; n < 58; n = n + 1)
+int n, k;
+for (n = 0; n < 10; n = n + 1)
 {
-  for (k = 48; k < 58;  k = k + 1)
+for (k = 0; k < 10;  k = k + 1)
 {
-putchar(n);
-putchar(k);
-if (n < 57 && k < 57)
+putchar(n + 48);
+putchar(k + 48);
+if (n * k != 81)
 {
 putchar(44);
 putchar(32);
