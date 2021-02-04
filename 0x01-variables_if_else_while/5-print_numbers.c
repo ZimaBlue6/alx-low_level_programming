f58c0fad21f3ee43466cfe53a86cac58c7dd7dac
Kidus Namaga <kidusrocks@gmail.com>
2021-02-04 17:25:52 +0000

    further on C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - outputs 0 to 9.
+ *Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n = n + 1)
+printf("%d", n);
+printf("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - outputs 0 to 9 using putchar.
+ *Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n = n + 1)
+putchar(n);
+putchar('\n');
+return (0);
+}
