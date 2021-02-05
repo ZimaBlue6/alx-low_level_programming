9a1ac653d88b7552e12fba29165e179b74168e04
Kidus Namaga <kidusrocks@gmail.com>
2021-02-05 13:21:41 +0000

    further on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char err_msg[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2,err_msg,sizeof(err_msg));
+char _g[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, _g, sizeof(_g));
 return (1);
 }
