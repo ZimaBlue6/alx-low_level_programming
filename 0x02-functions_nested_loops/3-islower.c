d5cfaff5ed2eb97b6e5b68a6107c93a25054a8d5
Kidus Namaga <kidusrocks@gmail.com>
2021-02-09 12:47:30 +0000

    checking if c is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *_islower - a function that checks for lowercase character.
- *@c: checking if character is lowercase 
+ *@c: checking if character is lowercase
  *Return: 1 if true and 0 if false
  */
 int _islower(int c)
