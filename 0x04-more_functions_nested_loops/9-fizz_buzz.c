4580babc13e45cbe9bbf07830bf7697bc6393cac
Kidus Namaga <kidusrocks@gmail.com>
2021-02-11 20:36:43 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Fizz-Buzz test
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n = n + 1)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+putchar('\n');
+return (0);
+}
