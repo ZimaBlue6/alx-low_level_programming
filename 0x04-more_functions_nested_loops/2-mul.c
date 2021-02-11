b174f197eec0c18f63005c94d210a8d27a320324
Kidus Namaga <kidusrocks@gmail.com>
2021-02-11 14:04:47 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *mul - a function that multiplies a and b
+ *@a: multiplicand
+ *@b: multiplier
+ *Return: 0
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+		return (c);
+}
