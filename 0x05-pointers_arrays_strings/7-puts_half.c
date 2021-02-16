61eb5e984bc6bd0ed8913db4e15e165d4c750965
Kidus Namaga <kidusrocks@gmail.com>
2021-02-16 18:58:12 +0000

    halfsies

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,6 @@
 #include "holberton.h"
 
 /**
- *_strlen - a function that returns the length of a string
- *@s: pointer
- *Return: 0
- */
-
-int _strlen(char *s)
-{
-	int len_str;
-
-	for (len_str = 0; s[len_str] != '\0'; len_str = len_str + 1)
-		;
-	return (len_str);
-}
-
-/**
  *puts_half - a function that prints half a string
  *@str: pointer
  *
@@ -23,16 +8,16 @@ int _strlen(char *s)
 
 void puts_half(char *str)
 {
+	int len_str, n;
 
-	int n;
+	for (len_str = 0; str[len_str] != '\0'; len_str = len_str + 1)
+		;
+	len_str = len_str + 1;
 
-	n = _strlen(str) + 1;
+	n = len_str;
 
 	for (n = n / 2; str[n] != '\0'; n = n + 1)
 	{
-		if (_strlen(str) % 2 != 0)
-			n = (n / 2) - 1;
-
 		_putchar(str[n]);
 	}
 	_putchar('\n');
