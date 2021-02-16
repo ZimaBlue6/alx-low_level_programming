f7f2788e5a738add2794a40baa7bd4f8815a9645
Kidus Namaga <kidusrocks@gmail.com>
2021-02-16 11:36:55 +0000

    every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *puts2 - a function that prints every other character
+ *@str: pointer
+ *
+ */
+
+void puts2(char *str)
+{
+
+	int len_str;
+
+	for (len_str = 0; str[len_str] != '\0'; len_str = len_str + 2)
+		_putchar(str[len_str]);
+	_putchar('\n');
+}
