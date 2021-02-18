0f72b99433f71e11e79ac492ae603a51d5157295
Kidus Namaga <kidusrocks@gmail.com>
2021-02-18 08:39:27 +0000

    adding two strings together

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,6 @@
 #include "holberton.h"
 
 /**
- *_strlen - a function that returns the length of a string
- *@s: pointer
- *Return: 0
- */
-
-int _strlen(char *s)
-{
-	int len_str;
-
-	for (len_str = 0; s[len_str] != '\0'; len_str = len_str + 1)
-		;
-	len_str++;
-	return (len_str);
-}
-
-/**
  *_strcat - a function that concatenates two strings
  *@dest: target to copy to
  *@src: source to copy from
@@ -25,12 +9,17 @@ int _strlen(char *s)
 
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = _strlen(dest);
+	int len_str;
 	int i;
 
-	for (i = 0; i < dest_len && src[i] != '\0'; i++)
+	for (len_str = 0; dest[len_str] != '\0'; len_str = len_str + 1)
+                ;
 
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[len_str + i] = src[i];
+	}
+	dest[len_str + i] = '\0';
+
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *_strlen - a function that returns the length of a string
+ *@s: pointer
+ *Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len_str;
+
+	for (len_str = 0; s[len_str] != '\0'; len_str = len_str + 1)
+		;
+
+	return (len_str);
+}
+
+/**
+ *_strncat - a function that concatenates two strings up to n
+ *@dest: target to copy to
+ *@src: source to copy from
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
