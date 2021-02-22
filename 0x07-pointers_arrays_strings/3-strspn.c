ee4ae2044e77e48e6768dc609bada4bed4c55aba
Kidus Namaga <kidusrocks@gmail.com>
2021-02-22 14:54:00 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_strspn - a function that gets the length of a prefix substring
+ *@s: string to be scanned
+ *@accept: string containing the characters to match
+ *Return: the number of bytes in the initial segment of s which consist only of
+ *bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, len;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+				break;
+		}
+		if (accept[i])
+			len++;
+		else
+			break;
+		s++;
+	}
+	return (len);
+}
