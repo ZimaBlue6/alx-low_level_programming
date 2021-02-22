c28b6062bd41b0fd222515ef5cbbeaa9c1497d57
Kidus Namaga <kidusrocks@gmail.com>
2021-02-22 15:25:06 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strpbrk - a function that gets the length of a prefix substring
+ *@s: string to be scanned
+ *@accept: string containing the characters to match
+ *Return:  a pointer to the byte in s that matches one of the bytes in accept,
+ *or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	do {
+		for (i = 0; accept[i]; i++)
+		if (*s == accept[i])
+			return (s);
+	} while (*s++);
+
+	return (NULL);
+}
