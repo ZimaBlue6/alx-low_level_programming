f062a0abfea05c3f1f3d66ba7e2773085445d650
Kidus Tsedale <kidusrocks@gmail.com>
2021-02-27 14:18:33 +0000

    add+

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,9 +13,12 @@ int main(int argc, char *argv[])
 {
 	int i, addition = 0;
 
+	if (argc == 1)
+		printf("0\n");
+
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
