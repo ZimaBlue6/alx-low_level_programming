c5235ae88f7e2fddee285ecf154675cfecc64059
Kidus Tsedale <kidusrocks@gmail.com>
2021-02-27 13:47:28 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *main - a program that adds positive numbers
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, addition = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			addition = addition + atoi(argv[i]);
+	}
+	printf("%d\n", addition);
+	return (0);
+}
