4c38e9e7b8a003613614cb6bbbb4e6189dee50e0
Kidus Tsedale <kidusrocks@gmail.com>
2021-03-01 09:42:34 +0000

    after q&a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 
 /**
  *main - a program that adds positive numbers
@@ -9,14 +9,24 @@
  *Return: 0
  */
 
+int digit_check(char *s)
+{
+	while (*s)
+	{
+		if (!isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
 int main(int argc, char *argv[])
 {
 	int i, addition = 0;
 
-
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		if (!digit_check(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
