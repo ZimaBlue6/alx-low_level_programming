dd22c9e7e5725ce1952be66babd5ab3ab07fb065
Kidus Tsedale <kidusrocks@gmail.com>
2021-03-03 10:58:32 +0000

    args

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *argstostr - a function that concatenates all the arguments of your program
+ * alloc_grid function
+ *@ac: argument counter
+ *@av: argument vector
+ *Return: a pointer to a new string, or NULL if it fails
+**/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * len);
+
+	if (str == 0)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	return (str);
+}
